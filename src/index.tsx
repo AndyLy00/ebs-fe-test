285e6443acf85a516fc8c9d2eed5a0e40d919bb1
Marcel Lefter <marcellefter@outlook.com>
2022-07-21 23:17:39 +0300

    Update README.md

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render } from 'react-dom';
 
-render(<div>Hello World!</div>, document.getElementById('root'));
+render(<div></div>, document.getElementById('root'));
