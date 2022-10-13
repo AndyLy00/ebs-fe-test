d90385c3c3c7b21509ebea1a8e84b2b4d2cddd34
AndyLy00 <andreia.cristina.siretanu@gmail.com>
2022-10-13 23:59:53 +0300

    Merge remote-tracking branch 'origin/master'

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -5,7 +5,6 @@ import storeItems from "../data/data.json"
 export function Store() {
   return (
     <>
-      <h1>Store</h1>
       <Row md={1} xs={1} lg={1} className="g-3">
           {storeItems.map(item => (
               <Col key={item.id}>
